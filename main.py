e04eb75b25ea45151c71e9cecb903dbf4005d7ec
leftrightleft <leftrightleft@github.com>
2022-11-12 11:40:06 -0800

    try again

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ def set_action_output(output_name, value) :
     if "GITHUB_OUTPUT" in os.environ :
         with open(os.environ["GITHUB_OUTPUT"], "a") as f :
             print("{0}={1}".format(output_name, value), file=f)
-            for line in f:
-                print(line)
+    print("{0}={1}".format(output_name, value))
 
 def main():
     languages = get_languages()
